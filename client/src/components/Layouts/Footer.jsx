d5b6889d9b5a2ac29399030ff90108064cb1e776
bhu <bhu@pop-os.localdomain>
2021-10-14 12:27:12 +0530

    local

--- client/src/components/Layouts/Footer.jsx
+++ client/src/components/Layouts/Footer.jsx
@@ -5,6 +5,10 @@ import FacebookIcon from '@mui/icons-material/Facebook'
 import InstagramIcon from '@mui/icons-material/Instagram'
 import TwitterIcon from '@mui/icons-material/Twitter'
 import PinterestIcon from '@mui/icons-material/Pinterest'
+import RoomOutlinedIcon from '@mui/icons-material/RoomOutlined'
+import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined'
+import AlternateEmailOutlinedIcon from '@mui/icons-material/AlternateEmailOutlined';
+import CreditCardOutlinedIcon from '@mui/icons-material/CreditCardOutlined';
 
 const Container = styled.div`
   display: flex;
@@ -33,13 +37,36 @@ const SocialIcon = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-right: 20px
+  margin-right: 20px;
 `
 const Center = styled.div`
   flex: 1;
 `
+const Title = styled.h3`
+  margin-bottom: 30px;
+`
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`
+
 const Right = styled.div`
   flex: 1;
+`
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
 `
 
 const Footer = () => {
@@ -68,8 +95,33 @@ const Footer = () => {
           </SocialIcon>
         </SocialContainer>
       </Left>
-      <Center></Center>
-      <Right></Right>
+      <Center>
+        <Title>Useful Lists</Title>
+        <List>
+          <ListItem>sample link</ListItem>
+          <ListItem>sample link</ListItem>
+          <ListItem>sample link</ListItem>
+          <ListItem>sample link</ListItem>
+          <ListItem>sample link</ListItem>
+          <ListItem>sample link</ListItem>
+          <ListItem>sample link</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <RoomOutlinedIcon />
+          Address
+        </ContactItem>
+        <ContactItem>
+          <LocalPhoneOutlinedIcon />
+          Phone Number
+        </ContactItem>
+        <ContactItem>
+          <AlternateEmailOutlinedIcon />
+          email</ContactItem>
+          <CreditCardOutlinedIcon />
+      </Right>
     </Container>
   )
 }
